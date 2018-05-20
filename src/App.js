c954ef879625832def2c7957ecac23839756b7c4
Taimur Abdaal <taimurabdaal@gmail.com>
2018-05-20 15:28:22 +0100

    Add account details section

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import BidHistory from './BidHistory.js';
 import HighestBid from './HighestBid.js';
 import WinningBids from './WinningBids.js';
 import Web3NotFound from './Web3NotFound.js';
+import PlaceBidComponent from './PlaceBidComponent.js';
 import AccountNotFound from './AccountNotFound.js';
 import './App.css';
 import MetamaskStatus from './MetamaskStatus.js';
@@ -14,58 +15,6 @@ import constants from './constants'
 
 function getContract(web3) {
   return new web3.eth.Contract(compiledContract.abi, constants.CONTRACT_ADDRESS);
-}
-
-class PlaceBidComponent extends Component {
-  componentWillMount() {
-    const web3 = new Web3(window.web3.currentProvider);
-    const contract = getContract(web3);
-    this.setState({ web3, contract });
-
-    web3.eth.getAccounts().then(accounts => {
-      if (accounts.length > 0) {
-        const account = accounts[0];
-        this.setState({ account });
-        return web3.eth.getBalance(account);
-      }
-    }).then(balance => {
-      this.setState({ balance });
-    });
-  }
-
-  handleSubmit(values) {
-    this.state.contract.methods.placeBid(values.url).send({
-      from: this.state.account,
-      to: constants.CONTRACT_ADDRESS,
-      value: this.state.web3.utils.toWei(values.bid, 'ether'),
-    }, (err, transactionHash) => {
-      console.log(err);
-      console.log(transactionHash);
-    })
-  }
-
-  render () {
-    const account = this.state.account
-    const balance = this.state.balance ? this.state.web3.utils.fromWei((this.state.balance), 'ether') : 0
-
-    return !account ? (<p>loading account...</p>) : (
-      <div>
-        <p>account: {account}</p>
-        <p>balance: {balance} ETH</p>
-        <Form
-          onSubmit={this.handleSubmit.bind(this)}
-          render={({ submitForm }) => (
-            <form onSubmit={submitForm}>
-              <Text field="bid" placeholder='Bid (in ETH)' />
-              <Text field="url" placeholder='Url' />
-              <button type="submit">Submit</button>
-            </form>
-          )}
-        />
-      </div>
-    )
-
-  }
 }
 
 class App extends Component {
@@ -120,15 +69,13 @@ class App extends Component {
           <aside className="side">
             <h1>algo.app</h1>
             <h2>domain auction on the<br/>ethereum blockchain</h2>
-            <div className="spacer-20"/>
             <div className="faq">
               <div className="question">
                 how does it work?
               </div>
               <div className="answer">
-                This is a rolling auction — every 3 days, the highest bidder gets to choose where the algo.app domain goes to.
-                <br/><br/>
-                Bids can be placed anytime, by sending the bid amount in ETH to the auction’s smart contract (see below).
+                This is a rolling auction — every 24 hours, the highest bidder controls where the algo.app domain points to.
+                Bids can be placed anytime (see below).
               </div>
             </div>
             <div className="faq">
@@ -152,7 +99,7 @@ class App extends Component {
               accountUnavailableScreen={AccountNotFound}
             >
               <MetamaskStatus status='connected'/>
-              <PlaceBidComponent />
+              <PlaceBidComponent minimumBid={highestBid.amount * 1.1}/>
             </Web3Provider>
           </aside>
           <article className="content">
---
+++ src/PlaceBidComponent.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import { Form, Text } from 'react-form';
+import { Web3Provider } from 'react-web3';
+import Web3 from 'web3';
+import Web3NotFound from './Web3NotFound.js';
+import AccountNotFound from './AccountNotFound.js';
+import MetamaskStatus from './MetamaskStatus.js';
+import compiledContract from './DomainAuction.json';
+import constants from './constants'
+import './PlaceBidComponent.css';
+
+const ethereumLogo = require('./ethereum-logo.svg');
+
+function getContract(web3) {
+    return new web3.eth.Contract(compiledContract.abi, constants.CONTRACT_ADDRESS);
+}
+
+class PlaceBidComponent extends Component {
+    componentWillMount() {
+      const web3 = new Web3(window.web3.currentProvider);
+      const contract = getContract(web3);
+      this.setState({ web3, contract });
+  
+      web3.eth.getAccounts().then(accounts => {
+        if (accounts.length > 0) {
+          const account = accounts[0];
+          this.setState({ account });
+          return web3.eth.getBalance(account);
+        }
+      }).then(balance => {
+        this.setState({ balance });
+      });
+    }
+  
+    handleSubmit(values) {
+      this.state.contract.methods.placeBid(values.url).send({
+        from: this.state.account,
+        to: constants.CONTRACT_ADDRESS,
+        value: this.state.web3.utils.toWei(values.bid, 'ether'),
+      }, (err, transactionHash) => {
+        console.log(err);
+        console.log(transactionHash);
+      })
+    }
+  
+    render () {
+      const account = this.state.account
+      const balance = this.state.balance ? this.state.web3.utils.fromWei((this.state.balance), 'ether') : 0
+      const minimumBid = this.props.minimumBid || 0;
+  
+      return !account ? (<p>loading account...</p>) : (
+        <div>
+          <div className="account">
+              <div className="account-address">
+                <div className="account-heading">account address</div>
+                <span className="address">{account}</span>
+              </div>
+              <div className="account-balance">
+                <div className="account-heading">balance</div>
+                <span className="balance">{Number(balance).toPrecision(2)} ETH</span>
+              </div>
+              <img src={ethereumLogo} className="ethereum-logo"/>
+          </div>
+          <p>min bid: {minimumBid} ETH</p>
+          <Form
+            onSubmit={this.handleSubmit.bind(this)}
+            render={({ submitForm }) => (
+              <form onSubmit={submitForm}>
+                <Text field="bid" placeholder='Bid (in ETH)' />
+                <Text field="url" placeholder='Url' />
+                <button type="submit">Submit</button>
+              </form>
+            )}
+          />
+        </div>
+      )
+  
+    }
+  }
+
+  export default PlaceBidComponent;
