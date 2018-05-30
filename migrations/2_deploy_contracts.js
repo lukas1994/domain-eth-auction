5dbeb3350e1a7a5822716e3c581de149acb028e1
Lukas Koebis <lukask@whatsapp.com>
2018-05-29 22:39:04 -0700

    empty

--- migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-const DomainAuction = artifacts.require("./DomainAuction.sol");
-const FailingContract = artifacts.require("./FailingContract.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(DomainAuction);
-  deployer.deploy(FailingContract);
-};
